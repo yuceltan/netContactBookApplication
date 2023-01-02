a4f582ca295af1de512aba8b15d633cbee3ec421
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-01-02 20:02:41 +0300

    save contact page and database connection is maden succesfully without list of all contacts

---
+++ netContactBookApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using netContactBookApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace netContactBookApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ContactRecord cot)
+        {
+            if (ModelState.IsValid)
+            {
+                yuceltandbEntities db = new yuceltandbEntities();
+                db.ContactRecords.Add(cot);
+                db.SaveChanges();
+            }
+
+
+
+            return View();
+        }
+    }
+}
