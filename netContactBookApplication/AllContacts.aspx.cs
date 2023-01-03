e7ed9be6158ae3bf7f7aec5f5482b037882a9ec1
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-01-03 22:35:47 +0300

    Page of all contacts is created and added all contacts with their buttons (search,edit,birthday,)

---
+++ netContactBookApplication/AllContacts.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace netContactBookApplication
+{
+    public partial class AllContacts : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+                
+
+        }
+        protected void AddNewContact_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Index.cshtml");
+        }
+        
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        }
+    }
--- netContactBookApplication/Controllers/HomeController.cs
+++ netContactBookApplication/Controllers/HomeController.cs
@@ -33,6 +33,8 @@ namespace netContactBookApplication.Controllers
             return View();
         }
 
+        
+        
 
     }
 }
